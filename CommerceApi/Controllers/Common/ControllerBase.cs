03841a8352642ac88b7c6e6c75f09d0951786c8f
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-11-14 22:53:25 +0100

    Add swagger documentation

---
+++ CommerceApi/Controllers/Common/ControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OperationResults;
+using OperationResults.AspNetCore;
+
+namespace CommerceApi.Controllers;
+
+public abstract class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+{
+    protected ControllerBase()
+    {
+    }
+
+    protected IActionResult CreateResponse(Result result, int? successStatusCode = null)
+    {
+        return HttpContext.CreateResponse(result, successStatusCode);
+    }
+
+    protected IActionResult CreateResponse<T>(Result<T> result, int? successStatusCode = null)
+    {
+        return HttpContext.CreateResponse(result, successStatusCode);
+    }
+}
--- CommerceApi/Program.cs
+++ CommerceApi/Program.cs
@@ -1,12 +1,180 @@
+using System.Net.Mime;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CommerceApi.Documentation;
+using CommerceApi.Security;
+using FluentValidation.AspNetCore;
+using Hellang.Middleware.ProblemDetails;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using OperationResults.AspNetCore;
+using Serilog;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using TinyHelpers.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigureServices(builder.Services, builder.Configuration, builder.Host);
 
 var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+Configure(app, provider);
+
+await app.RunAsync();
+
+void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostBuilder host)
+{
+    host.UseSerilog((hostingContext, loggerConfiguration) =>
+    {
+        loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+    });
+
+    services.AddProblemDetails(options =>
+    {
+        options.Map<Exception>(ex => new StatusCodeProblemDetails(StatusCodes.Status500InternalServerError));
+    });
+    services.AddMemoryCache();
+    services.AddOperationResult();
+
+    services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+        options.JsonSerializerOptions.Converters.Add(new TimeSpanTicksConverter());
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+    services.AddApiVersioning(options =>
+    {
+        options.ReportApiVersions = true;
+    });
+    services.AddVersionedApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
+
+    services.AddEndpointsApiExplorer();
+
+    services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+    services.AddSwaggerGen(options =>
+    {
+        options.OperationFilter<SwaggerDefaultValues>();
+
+        options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Insert JWT token with the \"Bearer \" prefix",
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = JwtBearerDefaults.AuthenticationScheme
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        options.IncludeXmlComments(xmlPath);
+
+        options.CustomOperationIds(api => $"{api.ActionDescriptor.RouteValues["controller"]}_{api.ActionDescriptor.RouteValues["action"]}");
+        options.UseAllOfToExtendReferenceSchemas();
+    })
+    .AddFluentValidationRulesToSwagger(options =>
+    {
+        options.SetNotNullableIfMinLengthGreaterThenZero = true;
+    });
+
+    services.AddFluentValidationAutoValidation(options =>
+    {
+        options.DisableDataAnnotationsValidation = true;
+    });
+
+    var hashedConnectionString = configuration.GetConnectionString("SqlConnection");
+    var connectionString = StringHasher.GetString(hashedConnectionString);
+
+    services.AddHealthChecks().AddAsyncCheck("sql", async () =>
+    {
+        try
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy(ex.Message, ex);
+        }
+
+        return HealthCheckResult.Healthy();
+    });
+}
+
+void Configure(IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+{
+    app.UseProblemDetails();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
+            options.RoutePrefix = string.Empty;
+        }
+    });
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseCors();
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.IncludeQueryInRequestPath = true;
+    });
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+
+        endpoints.MapHealthChecks("/status",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new
+                            {
+                                Status = report.Status.ToString(),
+                                Details = report.Entries.Select(e => new
+                                {
+                                    Service = e.Key,
+                                    Status = Enum.GetName(typeof(HealthStatus), e.Value.Status),
+                                    Description = e.Value.Description
+                                })
+                            });
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+    });
+}
