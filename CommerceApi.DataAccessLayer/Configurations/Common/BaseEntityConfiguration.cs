662575c601f964357c95857320344c85ca17e8b0
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-11-15 22:25:17 +0100

    fix error

--- CommerceApi.DataAccessLayer/Configurations/Common/BaseEntityConfiguration.cs
+++ CommerceApi.DataAccessLayer/Configurations/Common/BaseEntityConfiguration.cs
@@ -9,7 +9,7 @@ public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> w
     public virtual void Configure(EntityTypeBuilder<T> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.Property(x => x.Id).ValueGeneratedOnAddOrUpdate();
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
 
         builder.Property(x => x.CreationDate).IsRequired();
         builder.Property(x => x.UpdatedDate).IsRequired(false);
