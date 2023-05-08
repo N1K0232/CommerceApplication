28e20944253147b57fc3ce1e04c1482578634115
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-08 21:19:27 +0200

    fix nullable reference types warnings

--- src/CommerceApi.Shared/Models/Category.cs
+++ src/CommerceApi.Shared/Models/Category.cs
@@ -4,7 +4,7 @@ namespace CommerceApi.Shared.Models;
 
 public class Category : BaseModel
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 }
