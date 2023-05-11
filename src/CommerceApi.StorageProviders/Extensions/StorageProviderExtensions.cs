66e1f767652e0bb7f0ef393c1540302a198c4d47
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-11 21:34:47 +0200

    Add extensions method for IStorageProvider interface

---
+++ src/CommerceApi.StorageProviders/Extensions/StorageProviderExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace CommerceApi.StorageProviders.Extensions;
+
+public static class StorageProviderExtensions
+{
+    public static async Task<byte[]> ReadAsByteAsync(this IStorageProvider storageProvider, string path)
+    {
+        using var input = await storageProvider.ReadAsync(path).ConfigureAwait(false);
+        if (input is null)
+        {
+            return null;
+        }
+
+        var content = new Memory<byte>();
+        await input.ReadAsync(content).ConfigureAwait(false);
+
+        var output = content.ToArray();
+        return output;
+    }
+
+    public static async Task<string> ReadAsStringAsync(this IStorageProvider storageProvider, string path)
+    {
+        using var input = await storageProvider.ReadAsync(path).ConfigureAwait(false);
+        if (input is null)
+        {
+            return null;
+        }
+
+        using var reader = new StreamReader(input);
+
+        var content = await reader.ReadToEndAsync().ConfigureAwait(false);
+        return content;
+    }
+
+    public static Task UploadAsync(this IStorageProvider storageProvider, string path, byte[] content)
+    {
+        var stream = new MemoryStream(content);
+        return storageProvider.UploadAsync(path, stream);
+    }
+}
