3486fcb18c0c49017e9d05d3fad00bb510675fda
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-13 14:24:40 +0200

    Add service to create and upload a pdf document

---
+++ src/CommerceApi.BusinessLayer/RemoteServices/Interfaces/IPdfService.cs
@@ -0,0 +1,8 @@
+﻿using CommerceApi.DataAccessLayer.Entities;
+
+namespace CommerceApi.BusinessLayer.RemoteServices.Interfaces;
+
+public interface IPdfService
+{
+    Task UploadPdfInvoiceAsync(Invoice invoice);
+}
---
+++ src/CommerceApi.BusinessLayer/RemoteServices/PdfService.cs
@@ -0,0 +1,49 @@
+﻿using CommerceApi.BusinessLayer.RemoteServices.Interfaces;
+using CommerceApi.DataAccessLayer.Entities;
+using CommerceApi.StorageProviders;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace CommerceApi.BusinessLayer.RemoteServices;
+
+public class PdfService : IPdfService
+{
+    private readonly IStorageProvider storageProvider;
+
+    public PdfService(IStorageProvider storageProvider)
+    {
+        this.storageProvider = storageProvider;
+    }
+
+    public async Task UploadPdfInvoiceAsync(Invoice invoice)
+    {
+        var pdfStream = new MemoryStream();
+        var document = new Document(PageSize.A4);
+        var writer = PdfWriter.GetInstance(document, pdfStream);
+
+        document.Open();
+
+        var table = new PdfPTable(5);
+        var phrase = new Phrase("Invoices");
+
+        var cell = new PdfPCell(phrase)
+        {
+            Colspan = 2,
+            HorizontalAlignment = 1
+        };
+
+        table.AddCell(cell);
+        table.AddCell(invoice.Product.Name);
+        table.AddCell(invoice.Price.ToString());
+        table.AddCell(invoice.Quantity.ToString());
+        table.AddCell(invoice.TotalPrice.ToString());
+
+        document.Add(table);
+
+        document.Close();
+        writer.Close();
+
+        pdfStream.Position = 0;
+        await storageProvider.SaveAsync(invoice.DownloadFileName, pdfStream);
+    }
+}
