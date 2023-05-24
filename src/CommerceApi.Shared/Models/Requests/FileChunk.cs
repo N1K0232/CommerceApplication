56c672ad1bf2dd463e4b9a5637c8b5ae43ea754b
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-24 17:48:20 +0200

    Add models for chunk file uploading

---
+++ src/CommerceApi.Shared/Models/Requests/FileChunk.cs
@@ -0,0 +1,10 @@
+﻿namespace CommerceApi.Shared.Models.Requests;
+
+public class FileChunk
+{
+    public string FileHandle { get; set; } = null!;
+
+    public string Data { get; set; } = null!;
+
+    public long StartAt { get; set; }
+}
