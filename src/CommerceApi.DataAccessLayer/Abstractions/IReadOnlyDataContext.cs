ce69682e459216d090d4727b086ed6cb72a7b49a
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-26 14:18:00 +0200

    change GetAsync method signature

--- src/CommerceApi.DataAccessLayer/Abstractions/IReadOnlyDataContext.cs
+++ src/CommerceApi.DataAccessLayer/Abstractions/IReadOnlyDataContext.cs
@@ -4,7 +4,7 @@ namespace CommerceApi.DataAccessLayer.Abstractions;
 
 public interface IReadOnlyDataContext
 {
-    ValueTask<TEntity> GetAsync<TEntity>(Guid id) where TEntity : BaseEntity;
+    ValueTask<TEntity> GetAsync<TEntity>(params object[] keyValues) where TEntity : BaseEntity;
 
     IQueryable<TEntity> GetData<TEntity>(bool ignoreQueryFilters = false, bool trackingChanges = false) where TEntity : BaseEntity;
 }
