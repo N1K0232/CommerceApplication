af101e856fe782caa771271fc6e5af937ff29583
N1K0232 <ns.nicolasilvestri@libero.it>
2023-06-17 15:46:53 +0200

    file rename for SqlContext file to CommerceApplicationSqlContext

--- src/CommerceApi.DataAccessLayer/CommerceApplicationSqlContext.cs
+++ src/CommerceApi.DataAccessLayer/CommerceApplicationSqlContext.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 
 namespace CommerceApi.DataAccessLayer;
 
-public class CommerceApplicationSqlContext : ISqlContext
+public class CommerceApplicationSqlContext : ICommerceApplicationSqlContext
 {
     private SqlConnection _connection = null;
     private CancellationTokenSource _tokenSource = null;
