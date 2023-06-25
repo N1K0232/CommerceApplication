158ab700fa973111d673a9cd231c4721c690f881
N1K0232 <ns.nicolasilvestri@libero.it>
2023-06-25 18:31:44 +0200

    refactoring to double controller for authentication

---
+++ src/CommerceApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,78 @@
+﻿using CommerceApi.Authentication.Common;
+using CommerceApi.Authentication.Extensions;
+using CommerceApi.Authorization.Filters;
+using CommerceApi.BusinessLayer.Services.Interfaces;
+using CommerceApi.Models;
+using CommerceApi.Shared.Models.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceApi.Controllers;
+
+public class AuthenticationController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AuthenticationController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpDelete("DeleteAccount")]
+    [RoleAuthorize(RoleNames.Administrator, RoleNames.PowerUser)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteAccount(Guid userId)
+    {
+        var result = await _userService.DeleteAccountAsync(userId);
+        return CreateResponse(result, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("Login")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var response = await _userService.LoginAsync(request);
+        return CreateResponse(response, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("Register")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var response = await _userService.RegisterAsync(request);
+        return CreateResponse(response, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("Refresh")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest request)
+    {
+        var response = await _userService.RefreshTokenAsync(request);
+        return CreateResponse(response, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("SignOut")]
+    public async Task<IActionResult> Signout()
+    {
+        var email = User.GetEmail();
+
+        var result = await _userService.SignOutAsync(email);
+        return CreateResponse(result, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("uploadimage")]
+    [Consumes("multipart/form-data")]
+    [RoleAuthorize(RoleNames.Administrator, RoleNames.PowerUser, RoleNames.User)]
+    public async Task<IActionResult> UploadPhoto([FromForm] FormFileContent content)
+    {
+        var result = await _userService.UploadPhotoAsync(User.GetId(), content.File.FileName, content.GetFileStream());
+        return CreateResponse(result, StatusCodes.Status200OK);
+    }
+}
