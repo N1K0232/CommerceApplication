ea66b65ae23e47565fb719a395ac8cad02508a45
N1K0232 <ns.nicolasilvestri@libero.it>
2023-07-06 20:09:26 +0200

    Add abstract classes for DocumentFilter and OperationFilter objects

---
+++ src/CommerceApi.Swagger/Abstractions/DocumentFilter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CommerceApi.Swagger.Abstractions;
+
+public abstract class DocumentFilter : IDocumentFilter
+{
+    public abstract void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context);
+}
