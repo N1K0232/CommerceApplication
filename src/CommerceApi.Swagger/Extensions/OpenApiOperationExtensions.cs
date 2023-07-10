d941fe0bfb032cd6e58b37f6ce808dcde412d066
N1K0232 <ns.nicolasilvestri@libero.it>
2023-07-10 13:57:02 +0200

    to be completed: Add extension method to create a OpenApiResponse on Swagger

---
+++ src/CommerceApi.Swagger/Extensions/OpenApiOperationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CommerceApi.Swagger.Extensions;
+
+public static class OpenApiOperationExtensions
+{
+    public static void AddResponse(this OpenApiOperation operation, string key, string description)
+    {
+        var response = CreateResponse(description);
+        operation.Responses.Add(key, response);
+    }
+
+    private static OpenApiResponse CreateResponse(string description)
+    {
+        var response = new OpenApiResponse { Description = description };
+        return response;
+    }
+}
