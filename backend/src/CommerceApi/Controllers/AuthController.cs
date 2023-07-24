c43cf2f06eb26f9a0b57385174c1fd786e2fbc84
N1K0232 <ns.nicolasilvestri@libero.it>
2023-07-24 16:55:25 +0200

    fixes

---
+++ backend/src/CommerceApi/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using CommerceApi.BusinessLayer.Services.Interfaces;
+using CommerceApi.Shared.Models.Requests;
+using CommerceApi.Shared.Models.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceApi.Controllers;
+
+public class AuthController : ControllerBase
+{
+    private readonly IIdentityService _identityService;
+
+    public AuthController(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    [HttpPost("register")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+    {
+        var registerResult = await _identityService.RegisterAsync(registerRequest);
+        return CreateResponse(registerResult, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+    {
+        var loginResult = await _identityService.LoginAsync(loginRequest);
+        return CreateResponse(loginResult, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("refresh")]
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest refreshTokenRequest)
+    {
+        var refreshTokenResult = await _identityService.RefreshTokenAsync(refreshTokenRequest);
+        return CreateResponse(refreshTokenResult, StatusCodes.Status200OK);
+    }
+}
---
+++ backend/src/CommerceApi/Controllers/MeController.cs
@@ -0,0 +1,72 @@
+﻿using CommerceApi.Authentication.Common;
+using CommerceApi.Authorization.Filters;
+using CommerceApi.BusinessLayer.Services.Interfaces;
+using CommerceApi.Shared.Models;
+using CommerceApi.Shared.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceApi.Controllers;
+
+public class MeController : ControllerBase
+{
+    private readonly IAuthenticatedService _authenticatedService;
+
+    public MeController(IAuthenticatedService authenticatedService)
+    {
+        _authenticatedService = authenticatedService;
+    }
+
+    [HttpPost("youraddress")]
+    [RoleAuthorize(RoleNames.User)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddAddress([FromBody] SaveAddressRequest address)
+    {
+        var result = await _authenticatedService.AddAddressAsync(address);
+        return CreateResponse(result, StatusCodes.Status200OK);
+    }
+
+    [HttpPost("addphoto")]
+    [RoleAuthorize(RoleNames.User)]
+    [Consumes("multipart/form-data")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddPhoto(IFormFile file)
+    {
+        var addPhotoResult = await _authenticatedService.AddPhotoAsync(file.FileName, file.OpenReadStream());
+        return CreateResponse(addPhotoResult, StatusCodes.Status200OK);
+    }
+
+    [HttpGet("accountinfo")]
+    [RoleAuthorize(RoleNames.Administrator, RoleNames.PowerUser, RoleNames.User, RoleNames.Customer)]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Get()
+    {
+        var user = await _authenticatedService.GetAsync();
+        return Ok(user);
+    }
+
+    [HttpGet("getphoto")]
+    [RoleAuthorize(RoleNames.User)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetPhoto()
+    {
+        var contentResult = await _authenticatedService.GetPhotoAsync();
+        return CreateResponse(contentResult, StatusCodes.Status200OK);
+    }
+}
